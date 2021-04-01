dd1f0748a262fa87cdac46823139091ba8861080
Selfira <81574694+Selfira17@users.noreply.github.com>
2021-04-01 21:03:31 +0700

    Add files via upload

---
+++ PBO_Latihan57_Vehicle.java
@@ -0,0 +1,57 @@
+/*
+Nama    : Selfira Anyuniarwati
+Kelas   : PBO (FS112B)
+NIM     : 2031332802
+Deskripsi : Latihan 57-Vehicle
+ */
+package pbo_latihan57_vehicle;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PBO_Latihan57_Vehicle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    System.out.println("Bicycle");
+    
+    String brand = "Trek Bike";
+    String Model = "7.4FX";
+    int gear = 23;
+    
+    
+    System.out.println("Brand   :"+brand);
+    System.out.println("Model   :"+Model);
+    System.out.println("Jumlah Gear   :"+gear);
+    
+    System.out.println("=======================================================");
+    
+    
+    System.out.println("SkateBoard");
+    
+    
+    String brand1 = "Ally Skate";
+    String Model1 = "Rocket";
+    String Board = "54.5";
+    
+    System.out.println("Brand   :"+brand1);
+    System.out.println("Model   :"+Model1);
+    System.out.println("Panjangnya Board   :"+Board);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
